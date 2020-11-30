cadbcd6792a0a2488d2d21e6604725a223b050b8
touregeldi <tore.kabi@gmail.com>
2020-12-01 02:06:46 +0500

    init commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+require('./db/mongoose')
+
+const Task = require('./models/tasks')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log('Server is upon port ' + port);
+})
